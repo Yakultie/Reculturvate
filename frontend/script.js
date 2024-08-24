07dac22d145a606fb1e149dcb3b162be83cddf4f
Tom Nguyen <nngu0985@uni.sydney.edu.au>
2024-08-24 22:52:50 +1000

    "yes"

--- frontend/script.js
+++ frontend/script.js
@@ -1,24 +1,12 @@
 // script.js
-function showNextQuestion(nextQuestionId) {
+function selectOption(selectedValue, nextQuestionId) {
+    // Optionally, you can store the selectedValue if needed
+
     const currentQuestion = document.querySelector('.question:not(.hidden)');
     const nextQuestion = document.getElementById(nextQuestionId);
 
-    if (currentQuestion) {
+    if (currentQuestion && nextQuestion) {
         currentQuestion.classList.add('hidden');
-    }
-
-    if (nextQuestion) {
         nextQuestion.classList.remove('hidden');
-
-        const questionText = nextQuestion.querySelector('.question-text');
-        const textContent = questionText.textContent;
-        questionText.innerHTML = '';
-
-        textContent.split('').forEach((char, index) => {
-            const span = document.createElement('span');
-            span.textContent = char;
-            span.style.setProperty('--char-index', index);
-            questionText.appendChild(span);
-        });
     }
 }
