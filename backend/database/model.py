94a3c8d501b3476ec72c3b8b7674e9031634fcb9
Yanney Ou <Yanney.ou@gmail.com>
2024-08-24 22:50:13 +1000

    Add CreateNewEmployee function

--- backend/database/model.py
+++ backend/database/model.py
@@ -48,7 +48,28 @@ def createNewCompany(company_name, admin_full_name, admin_email, password):
 
     return True
 
-def createNewEmployee(employee_name, employee_email, employee_company, linkedin_url, pronouns):
+def createNewEmployee(employee_name, employee_email, employee_password, employee_company, linkedin_url):
+    found_employee_email = users.find_one({"email": employee_email})
+    found_employee_company = companies.find_one({"company_name": employee_company})
     
+    if found_employee_email:
+        print(employee_email + " is an existing email")
+        return False
 
-print(createNewCompany("ABC Inc.", "ABC Admin", "admin@abc.com", "password"))
+    if not found_employee_company:
+        print(employee_company + " is not an existing company")
+        return False
+    
+    users.insert_one(
+        {
+            "name": employee_name,
+            "company": employee_company,
+            "email": employee_email,
+            "password": employee_password,
+            "linkedin_url": linkedin_url,
+            "reports": []
+        }
+    )   
+    return True
+
+print(createNewEmployee("Yanney OU","yaedb@gmail.com","meowmeow","ABC Inc.", "yabsdjhb"))
